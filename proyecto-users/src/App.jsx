0c4daed2f2bb6fa74751b6acc70a5268cf25e833
Amarthhell <ljorge313@gmail.com>
2023-12-14 01:34:56 -0400

    modificacion de codigo1

--- proyecto-users/src/App.jsx
+++ proyecto-users/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import logo from './assets/logo.png'
+import logo2 from './assets/archivando.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
     <>
-      <div>
-      <>
-  <meta charSet="UTF-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <div>
+       <>
+  
   <title>Login</title>
   <link rel="stylesheet" href="style.css" />
   <div className="ArchiByte_log">
-    <img src="ArchiByte_logo.png" style={{ width: 175 }} alt="Icono" />
+    <img src={logo} style={{ width: 175 }} alt="Icono" />
+  </div>
+  <div className="archivando">
+    <img src={logo2} style={{ width: 800 }} alt="Icono" />
   </div>
   <div className="formulario">
     <h1>ArchiByte</h1>
@@ -38,12 +38,13 @@ function App() {
   </div>
 </>
 
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+       </div>
     </>
-  )
+    );
 }
+  
+
+  
 
 export default App
