b499ad3d8d4efe726e59485fa905154c06a82ee6
Amarthhell <ljorge313@gmail.com>
2023-12-26 19:07:29 -0400

    anadiendo el backend

---
+++ proyecto-users/backend/src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask , request
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/usuarios"
+mongo = PyMongo(app)
+
+@app.route('/users',methods=['POTS'])
+def create_users():
+    username=request.json['username']
+    password=request.json['password']
+    email=request.json['email']
+    if username and password and email:
+        hashed_passwoard =  generate_password_hash(password)
+        mongo.db.users
+        
+    else: 
+        {'message' : 'recived'}
+        
+    return{'message' : 'recived'}
+if __name__ == "__name__":
+    app.run(debug=True)
