8baf26003d2a2ff7dad5f6f0a080eda362da6d90
Amarthhell <ljorge313@gmail.com>
2023-12-27 20:22:51 -0400

    creacion del backend completada

--- proyecto-users/backend/src/app.py
+++ proyecto-users/backend/src/app.py
@@ -1,6 +1,8 @@
-from flask import Flask , request
+from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_util
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/usuarios'
@@ -13,13 +15,49 @@ def create_users():
     email=request.json['email']
     if username and password and email:
         hashed_passwoard =  generate_password_hash(password)
-        mongo.db.users
+        id = mongo.db.users.insert(
+            {'username':username,'password':hashed_passwoard,'email':email}   
+        )
+        reponse = {
+            'id': str(id),
+            'username' : username,
+            'password': hashed_passwoard,
+            'email': email
+        }
+        return reponse
         
     else: 
-        {'message' : 'recived'}
+        return not_found()
    
         
     return{'message' : 'recived'}
+@app.errorhandler(404)
+def not_found(error=None):
+    respuesta = jsonify({
+        'messaje': 'Resource Not Found: ' + request.url,
+        'status': 404
+    })
+    respuesta.status_code = 404
+    return respuesta
+
+@app.route('/users',methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    response = json_util.dumps(users)
+    return Response(response, mimetype='application/json')
+
+@app.route('/users/<id>',methods=['GET'])
+def get_user():
+    user = mongo.db.users.find_one({'_id': ObjectId(id)})
+    respuesta = json_util.dumps(user)
+    return Response(respuesta, mimetype="application/json")
+    
+@app.route('/users/<id>',methods=['DELETE'])
+def delete_user(id):
+    mongo.db.users.delete_one({'_id': ObjectId(id)})
+    repuesta =jsonify({'massage':'user'+ id + 'fue eliminado corectamente'})
+    return repuesta
+
 
 
 if __name__ == "__main__":
